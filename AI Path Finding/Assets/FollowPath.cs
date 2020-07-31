2938140457eda53504255a4beb2405b34406fdcc
Mohammad-Fahim-Hossain <mohammad.fahim@northsouth.edu>
2020-07-31 14:48:15 +0600

    Upload the project.

---
+++ AI Path Finding/Assets/FollowPath.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    
+    public GameObject wpManager;
+    GameObject[] wps;
+    UnityEngine.AI.NavMeshAgent Agent;
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wps = wpManager.GetComponent<WayPoinManager>().waypoints;
+        Agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    public void GoToHeli() {
+        Agent.SetDestination(wps[1].transform.position);
+        //g.AStar(CurrentNode, wps[1]);
+       // currentWP = 0;
+    }
+
+    public void GoToRuin() {
+        Agent.SetDestination(wps[6].transform.position);
+        // g.AStar(CurrentNode, wps[6]);
+        // currentWP = 0;
+    }
+    public void GoToHil()
+    {
+        Agent.SetDestination(wps[2].transform.position);
+        //   g.AStar(CurrentNode, wps[2]);
+        // currentWP = 0;
+    }
+
+
+    void LateUpdate()
+    {
+
+        
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
